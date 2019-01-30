6edb639826b4f6f436b7cd6946dc9cd6eb6364f9
Flora Dong <45699473+floradong@users.noreply.github.com>
2019-01-30 10:34:31 -0800

    Update oneten.py

--- onetoten/oneten.py
+++ onetoten/oneten.py
@@ -1,25 +1,23 @@
-WIN = "win" #You win
-LOSE = "lose" #You lose
-TIE = "tie" #You tied
-UNKNOWN = "unknown" #The hasn't ended yet and you don't know who is going to win
+WIN = "win" #winning
+LOSE = "lose" #losing
+TIE = "tie" #tied
+UNKNOWN = "unknown" #don't know who has won yet
 
 def primitive(position):
-	# "lose" if end of game, otherwise UNKNOWN
 	if position == 10:
 		return LOSE
 	else:
 		return UNKNOWN
-	#	return LOSE if position == 0 else UNKNOWN
+	#	return LOSE if position == 0 or else UNKNOWN bc game hasn't ended yet
 
 def generate_moves(position):
-	# "return a tuple of possible moves"
-	#If your position is one, you only take one to ensure your opponent is already in
-	#the losing position. 
+	#returns a list of possible moves
+	# if you're the first to go, you take one to ensure your opponent is in losing pos
 	if position == 1:
-		return (1,)
+		return [1]
 	else:
-		return (1,2)
+		return [1,2]
 
 def do_move(position, move):
-	# "given position and move, do move on position"
+	# do the move given the position and move
 	return position + move
